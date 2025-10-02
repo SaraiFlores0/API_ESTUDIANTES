062b95203d54fbbca6d2d32cb82376cbcafdaa23
Saraí Flores <saraiflores302@gmail.com>
2025-10-02 02:41:21 -0600

    Actualización en schemas.py

--- schemas.py
+++ schemas.py
@@ -8,7 +8,9 @@ class EstudianteBase(BaseModel):
     
     edad: int = Field(..., gt=0, description="Edad del estudiante. Debe ser mayor que 0.")
    
-    email: EmailStr = Field(..., description="Correo electrónico único del estudiante.")
+    correo: EmailStr = Field(..., description="Correo electrónico único del estudiante.")
+
+    telefono: str = Field(..., min_length=7, max_length=15, description="Número de teléfono del estudiante.")
     
     foto_url: HttpUrl = Field(..., description="URL de la foto del estudiante (de Firebase, etc.).")
     
@@ -24,8 +26,11 @@ class EstudianteCreate(EstudianteBase):
 class EstudianteUpdate(BaseModel):
     nombre: Optional[str] = Field(None, min_length=1, description="Nombre del estudiante (opcional).")
     edad: Optional[int] = Field(None, gt=0, description="Edad del estudiante (opcional). Debe ser mayor que 0.")
-    email: Optional[EmailStr] = Field(None, description="Correo electrónico (opcional). Debe ser único.")
+    correo: Optional[EmailStr] = Field(None, description="Correo electrónico (opcional). Debe ser único.")
+    telefono: Optional[str] = Field(None, min_length=7, max_length=15, description="Número de teléfono (opcional).")
     foto_url: Optional[HttpUrl] = Field(None, description="URL de la foto (opcional).")
+
+
 
 class Estudiante(EstudianteBase):
     id: int = Field(..., description="ID autoincremental del estudiante.")
